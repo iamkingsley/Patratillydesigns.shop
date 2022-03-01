2cd0a12608059620c725e4bbafe4356644642d9d
Derric <realbenneh@gmail.com>
2022-03-01 17:37:41 +0000

    changed the base URl to .env

--- src/framework/rest/utils/request.ts
+++ src/framework/rest/utils/request.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './get-token';
 
 const request = axios.create({
-  baseURL: 'https://patra-tilly-server.herokuapp.com/api/',
+  baseURL: process.env.NEXT_PUBLIC_REST_API_ENDPOINT,
   timeout: 30000,
   headers: {
     Accept: 'application/json',
