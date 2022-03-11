6e3a27a6faa91fed7a2d80fabe75b3d1f4a72000
Bernard Kingsley Codjoe <bernard.codjoe@ibit-soft.com>
2022-03-11 10:17:11 +0000

    logo, category & project grids finetuning

--- src/components/products/grids/grid-with-loader.tsx
+++ src/components/products/grids/grid-with-loader.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'next-i18next';
 import rangeMap from '@lib/range-map';
 const styles = {
   // standard: 'md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7',
-  standard: 'md:grid-cols-3 lg:grid-cols-4 gap-6',
+  standard: 'md:grid-cols-3 lg:grid-cols-4 gap-1',
   shop: 'lg:grid-cols-2 xl:grid-cols-4 3xl:grid-cols-6',
   minimal:
     'grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6',
