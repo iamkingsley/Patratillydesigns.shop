6c4799bdfef2bf2240083b2919bf0ae1143a9226
Derric <realbenneh@gmail.com>
2022-03-15 17:22:41 +0000

    footer and whatsapp feature

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,11 @@
 const Footer = () => {
   return (
     <>
-      <footer className="text-center lg:text-left bg-light text-gray-600">
-        <div className="mx-6 py-10 text-center md:text-left">
-          <div className="grid grid-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+      <footer className="text-center lg:text-left text-gray-600">
+        <div className="w-full p-6 text-center md:text-left bg-gray-100 text-gray-600 border-b border-gray-300">
+          <div className="grid grid-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <div className="">
-              <h6
-                className="
+              <h6 className="
             uppercase
             font-semibold
             mb-4
@@ -16,37 +15,14 @@ const Footer = () => {
             md:justify-start
           "
               >
-                {/* <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cubes"
-            className="w-4 mr-3" role="img" xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512">
-            <path fill="currentColor"
-              d="M488.6 250.2L392 214V105.5c0-15-9.3-28.4-23.4-33.7l-100-37.5c-8.1-3.1-17.1-3.1-25.3 0l-100 37.5c-14.1 5.3-23.4 18.7-23.4 33.7V214l-96.6 36.2C9.3 255.5 0 268.9 0 283.9V394c0 13.6 7.7 26.1 19.9 32.2l100 50c10.1 5.1 22.1 5.1 32.2 0l103.9-52 103.9 52c10.1 5.1 22.1 5.1 32.2 0l100-50c12.2-6.1 19.9-18.6 19.9-32.2V283.9c0-15-9.3-28.4-23.4-33.7zM358 214.8l-85 31.9v-68.2l85-37v73.3zM154 104.1l102-38.2 102 38.2v.6l-102 41.4-102-41.4v-.6zm84 291.1l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6zm240 112l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6z">
-            </path>
-          </svg> */}
                 PATRA TILLY DESIGNS
               </h6>
               <p>
-                Here you can use rows and columns to organize your footer
-                content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit.
+                Patra Tilly Designs is a clothing company known for its trendy looks at reasonable prices.
+                Shop the latest Patra Tilly Designs clothing collection, which includes both Bespoke Wear and Ready to Wear options.
               </p>
             </div>
-            <div className="">
-              <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
-                Products
-              </h6>
-              <p className="mb-4">
-                <a href="#!" className="text-gray-600">
-                  Dress
-                </a>
-              </p>
-              <p className="mb-4">
-                <a href="#!" className="text-gray-600">
-                  Other
-                </a>
-              </p>
-            </div>
-            <div className="">
+            <div className="mx-auto">
               <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
                 Useful links
               </h6>
@@ -65,13 +41,13 @@ const Footer = () => {
                   Contact
                 </a>
               </p>
-              <p>
+              <p className="mb-4">
                 <a href="#!" className="text-gray-600">
                   Help
                 </a>
               </p>
             </div>
-            <div className="">
+            <div className="mx-auto">
               <h6 className="uppercase font-semibold mb-4 flex justify-center md:justify-start">
                 Contact
               </h6>
@@ -150,16 +126,13 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="flex justify-center items-center lg:justify-between p-6 border-b border-gray-300 bg-gray-100">
+        <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 lg:justify-between p-6 border-b border-gray-300 bg-gray-300">
           <div className="text-center">
-            <span>© 2022 Copyright:</span>
+            <span>© {new Date().getFullYear()} Copyright:</span>
             <a className="text-gray-600 font-semibold" href="/">
               {' '}
               PATRA TILLY DESIGNS
             </a>
-          </div>
-          <div className="mr-12 hidden lg:block">
-            <span>Get connected with us on social networks:</span>
           </div>
           <div className="flex justify-center">
             <a href="#!" className="mr-6 text-gray-600">
