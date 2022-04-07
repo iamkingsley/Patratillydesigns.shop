c3c614079377f0cea6082a5245bbce77480f4975
Bernard Kingsley Codjoe <bernard.codjoe@ibit-soft.com>
2022-04-07 11:57:26 +0000

    about page fixed

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,20 +1,27 @@
 import { useTranslation } from 'next-i18next';
-import { Image } from '@components/ui/image';
+import cn from 'classnames';
 import { getLayout } from '@components/layouts/layout';
 import { useSettings } from '@components/settings/settings.context';
-import { getIcon } from '@lib/get-icon';
-import isEmpty from 'lodash/isEmpty';
-import * as socialIcons from '@components/icons/social';
 export { getStaticProps } from '@framework/ssr/common';
 
 export const AboutPage = () => {
   const { t } = useTranslation('common');
   const settings = useSettings();
-  const { aboutUs } = settings
+  const { aboutUs } = settings;
   return (
-    <div className="max-w-screen-lg w-full mx-auto bg-light mt-2 p-2"
-      dangerouslySetInnerHTML={{__html: aboutUs}}>
-    </div>
+    <section className="py-8 px-4 lg:py-10 lg:px-8 xl:py-14 xl:px-16 2xl:px-20">
+      <header className="text-center mb-8 md:mt-16 lg:mt-0">
+        <h1 className="font-bold text-xl md:text-2xl xl:text-3xl uppercase">
+          {t('common:nav-menu-about')}
+        </h1>
+      </header>
+      <div
+        className={cn('max-w-screen w-full mx-auto', {
+          'h-60': aboutUs.split(' ').length < 120,
+        })}
+        dangerouslySetInnerHTML={{ __html: aboutUs }}
+      ></div>
+    </section>
   );
 };
 AboutPage.getLayout = getLayout;
